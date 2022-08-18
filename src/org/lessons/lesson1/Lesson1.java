bdc668d3e32a8588d13c4eaab9f0ebd09e2a161c
Texnikq <killaq@yandex.ru>
2022-08-18 22:14:53 +0300

    фиксация изменений 4

--- src/org/lessons/lesson1/Lesson1.java
+++ src/org/lessons/lesson1/Lesson1.java
@@ -87,6 +87,8 @@ public class Lesson1 {
         System.out.println(word3.trim());
         System.out.println("тест 2");
         System.out.println("тест 3");
+        System.out.println("тест 4");
+
 
 
 
